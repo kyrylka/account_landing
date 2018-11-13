19d6576fe10ccf5dfb4fe859559fc3ce752723c4
Kyryl <tiner1992@gmail.com>
2018-11-13 14:10:57 +0200

    Fix of scroll jumping exident

--- src/main.js
+++ src/main.js
@@ -252,9 +252,7 @@ var count = 0;
           var hash = this.hash;
           $('html, body').animate({
             scrollTop: $(hash).offset().top - 140
-          }, 800, function() { 
-            window.location.hash = hash; 
-          });
+          }, 800);
         }
       });
     });
