578d7a5be924d6bb04d67a0b486910bf86c13e73
Kyryl <tiner1992@gmail.com>
2018-11-13 08:35:33 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,205 @@
+$('.slidingBlock:first-child').animate({
+    right:"+=180"},330, function() {
+        $('.textUnderSlidingBlock:nth-child(2)').animate({
+    opacity: "toggle"
+    },400, nextOne())
+    });
+    function nextOne(){
+    $('.slidingBlock:nth-child(3)').animate({
+    right:"+=180"},330, function() {
+        $('.textUnderSlidingBlock:nth-child(4)').animate({
+    opacity: "toggle"
+    },400)
+    });}
+    /*Place cicles in cicrle*/
+    placeInRound();
+    $(window).resize(function(){
+      placeInRound();  
+    });
+    function placeInRound(){
+      var elems = $('.circleBlock');
+      var maxH=0;
+      for(var i=0; i<elems.length; i++){
+        if(maxH<$(elems[i]).outerHeight()){
+          maxH=$(elems[i]).outerHeight();
+        }
+      }
+      var widthC;
+      if (window.matchMedia("(min-width: 1366px)").matches) {
+        widthC = 1366;
+      }else{
+        widthC = $(window).width();
+      }
+      if(widthC<maxH*3){
+        maxH=widthC;
+      }
+      var cx, cy;
+      cx = widthC/2;
+      cy = maxH*3/2;
+      var coords=[];
+      for(var j=0; j<360; j+=60){
+        /*console.log("maxH="+maxH+'||j='+j+'||cx='+cx+"||cy="+cy);*/
+        coords.push(calcTraversalPoint(maxH, j, cx, cy));
+      }
+      for(var i=0; i< elems.length; i++){
+        $(elems[i]).css({"left": coords[i].x+"px", "top": coords[i].y+"px"})
+      }
+      $('.circlesBlock').height(maxH*3);  
+    }
+    /*end place cirles in circle*/
+    /*Ration pic switcher*/
+    var textToConvert=['Monetization  tools','Accurate Analytics','Social Networking','Customer Support','Advanced Content Management'];
+    var globS, globE;
+    var size=622;
+    var charSpace = 0.67;
+    var picturesToText = ['https://d242fdlp0qlcia.cloudfront.net/uploads/2016/01/23191504/richtexteditor2.png','https://d242fdlp0qlcia.cloudfront.net/uploads/2016/01/23191859/icanhasURL-1024x598.png','https://d242fdlp0qlcia.cloudfront.net/uploads/2016/01/24025611/linkFIX.png','https://d242fdlp0qlcia.cloudfront.net/uploads/2016/01/23191859/linkinmarkdown.png','https://d242fdlp0qlcia.cloudfront.net/uploads/2016/01/23191718/copyimageaddress.png'];
+    
+    createSvg();
+    function createSvg(){
+      var colorOnHover = $('#insertHere').attr('data-colorHOver');
+      var colorInitial = '#01a6fe';
+      var svgImage ='<svg usemap="#clickmap" width="'+size+'" height="'+size+'">';  
+      var usedCLength=0;  
+      var blockSpace = whiteSpaceToAdd(charSpace, textToConvert);
+      for(var i=0; i<textToConvert.length; i++){
+        if(i!=0){
+          usedCLength+=textToConvert[i-1].length+blockSpace;
+        }    
+        let svgBlocks=init(textToConvert[i],usedCLength,charSpace);    
+        svgImage+='<g data-textg="'+picturesToText[i]+'"><path class="pathClick" fill="'+colorInitial+'" d="'+svgBlocks.pathArrow+'" data-angl="'+svgBlocks.middleAngl+'"/><path fill="none" id="textPath'+i+'" d="'+svgBlocks.pathText+'" /><text x="'+svgBlocks.textLength/2+'" y="0" text-anchor="middle" fill="white" ><textPath href="#textPath'+i+'">'+textToConvert[i]+'</textPath></text></g>';
+      }
+      svgImage+='</svg>';
+      $('#insertHere').append(svgImage);
+      var toRotateAngl = $('.pathClick').attr('data-angl');  
+      toRotateAngl = 90+parseFloat(toRotateAngl);  
+      $('#insertHere svg').css("transform","rotate("+toRotateAngl+"deg)");
+      jQuery(function($){
+        $('path').click(function(){
+          clickDone(this);
+        });
+        $('text').click(function(){
+          clickDone(this);
+        });
+        $('path').hover(function(){
+          $(this).css('fill',colorOnHover);
+          $('#insertHere').addClass('paused');
+        },function(){
+          $(this).css('fill',colorInitial);
+          $('#insertHere').removeClass('paused');
+        });
+        $('text').hover(function(){
+          var path=$(this).parent().children('.pathClick');      
+          path.css('fill',colorOnHover);
+          $('#insertHere').addClass('paused');
+        },function(){
+          var path=$(this).parent().children('.pathClick');      
+          path.css('fill',colorInitial);
+          $('#insertHere').removeClass('paused');
+        });
+      });  
+    }
+    
+    function init(text,usedCLength,charSpace){
+      var textLineShift = 3; // in degrees
+      var charSize = parseFloat($('#insertHere').css("font-size"));
+      var textLength = text.length*charSpace*charSize;  
+      var middleCircleRadius = (size/2)-charSize;
+      var circleLength = 2*middleCircleRadius*Math.PI;  
+      var arcAnglStart = 360*(usedCLength*charSpace*charSize)/circleLength;
+      var arcAnglEnd = 360*(usedCLength*charSpace*charSize+textLength)/circleLength;  
+      var startPoint = {"outerRad": calcTraversalPoint((size/2), arcAnglStart, size), "middelRad": calcTraversalPoint(middleCircleRadius+charSize/4, arcAnglStart+textLineShift, size), "innerRad": calcTraversalPoint((middleCircleRadius-charSize), arcAnglStart, size),"middleRadExact":calcTraversalPoint(middleCircleRadius, arcAnglStart+textLineShift, size)};
+      globS=startPoint;
+      var endPoint = {"outerRad": calcTraversalPoint((size/2), arcAnglEnd, size), "middelRad": calcTraversalPoint(middleCircleRadius+charSize/4, arcAnglEnd+textLineShift, size), "innerRad": calcTraversalPoint((middleCircleRadius-charSize), arcAnglEnd, size),"middleRadExact":calcTraversalPoint(middleCircleRadius, arcAnglEnd+textLineShift, size)};
+      globE=endPoint;
+      var path1 = 'M '+startPoint.outerRad.x+' '+startPoint.outerRad.y+' A '+size/2+' '+size/2+' 0 0 0 '+ endPoint.outerRad.x +' '+endPoint.outerRad.y+' L '+endPoint.middleRadExact.x+' '+endPoint.middleRadExact.y+' L '+endPoint.innerRad.x+' '+endPoint.innerRad.y+' A '+((size/2) - (charSize*2))+' '+((size/2) - (charSize*2))+' 0 0 1 '+startPoint.innerRad.x+' '+ startPoint.innerRad.y +' L '+startPoint.middleRadExact.x+' '+startPoint.middleRadExact.y+' Z '+startPoint.outerRad.x+' '+startPoint.outerRad.y;
+      var pathText = 'M' + startPoint.middelRad.x +' '+ startPoint.middelRad.y + 'A'+(size/2-charSize)+' '+(size/2-charSize)+' 0 0 0 ' + endPoint.middelRad.x+ ' ' + endPoint.middelRad.y;  
+      return {'text': text, 'pathArrow': path1, 'pathText': pathText, 'charSize': charSize,'textLength':textLength,'middleAngl': (arcAnglStart+arcAnglEnd)/2};
+    }
+    
+    function whiteSpaceToAdd(charSpace, textToConvert){  
+      var ocuppiedLength =0;
+      for (var i=0; i<textToConvert.length; i++){
+        ocuppiedLength+=textToConvert[i].length;
+      }
+      let charSize = parseFloat($('#insertHere').css("font-size"));
+      let middleCircleRadius = (size/2)-charSize;
+      let circleLength = 2*middleCircleRadius*Math.PI;
+      let spaceCount = (circleLength-ocuppiedLength*charSpace*charSize)/textToConvert.length; 
+      return spaceCount/charSpace/charSize;
+    }
+    
+    function calcTraversalPoint(rad, angl, size, cy){
+      var x, y;
+      var a=size/2;
+      var xSign=1;    
+      if(angl>90&&angl< 270){
+        xSign=-1;
+      }
+      if(typeof cy!="undefined"){
+        a=size;
+      }
+      angl=angl*Math.PI/180;
+      x=Math.sqrt((rad*rad)/(1+Math.tan(angl)*Math.tan(angl)));
+      y=Math.tan(angl)*x;  
+      x=evenize(xSign*x+a);
+      if(typeof cy!="undefined"){
+        a=cy;
+      }
+      y=(-1)*(evenize(xSign*y-a));  
+      return {"x":x,"y":y};
+    }
+    
+    function evenize(num){
+      var rest=num%1;
+      if(rest<0.5){
+        num=num-rest;
+      }else{
+        num=(num-rest)+1;
+      }
+      return num;
+    }
+    
+    function clickDone(elem){
+      elem=$(elem);
+      var block=elem.closest('g');
+      block = block.attr('data-textg');
+      $('.imagesContainer img').fadeOut(300, function(){
+        $('.imagesContainer img').attr('src',block);
+        $('.imagesContainer img').fadeIn(300);
+      });
+      var anglToRot = parseFloat(elem.parent().find(".pathClick").attr('data-angl'));  
+      var delta = $('#insertHere svg').attr("style");
+      delta = parseFloat(delta.replace(/([^0-9,.-])/g,""));
+      var delta360=0;
+      if(delta>360){   
+        delta360 = (delta-delta%360);
+      }
+      var curPos=anglToRot-delta;
+      // small experiment
+      curPos=curPos+90;// 
+      var curPos360=curPos-(curPos%360);
+      curPos=curPos-curPos360;
+      var sign=1;
+      if(curPos<0){
+        sign=-1;
+      }
+      curPos=Math.abs(curPos);
+      if(curPos>180){
+        anglToRot=(-1)*(360-curPos);
+      } else {
+        anglToRot=curPos;
+      }
+      anglToRot=anglToRot*sign;  
+      /*
+      console.log("curPos="+curPos);  
+      if(curPos>90&& curPos<270){
+        anglToRot=-270+curPos;      
+      }else if(curPos>=270) {
+        anglToRot = curPos-270;
+      }else if(curPos<=90){
+        anglToRot = 90+curPos;
+      }*/
+      console.log('curPos='+curPos+'|| anglToRot='+anglToRot+" || delta="+delta);
+      $('#insertHere svg').css("transform","rotate("+(anglToRot+delta)+"deg)");  
+    }
+    
