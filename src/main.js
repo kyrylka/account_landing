fb333f8dd7e30858ba1679fafe023bbda1e1bd75
Kyryl <tiner1992@gmail.com>
2018-11-14 11:41:52 +0200

    Bug fix

--- src/main.js
+++ src/main.js
@@ -281,7 +281,7 @@ var count = 0;
       for(var i=0; i<i.length; i++){
         requestDemoData[$(mass[i]).attr('name')]=$(mass[i]).val();
       }
-      var keys = ObjectKeys(requestDemoData);
+      var keys = Object.keys(requestDemoData);
       for(var i=0; i<keys.length; i++){
         if(requestDemoData[keys[i]]===''){
           requestDemoData[keys[i]]='Not specified';
